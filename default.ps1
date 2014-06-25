c4df28fd1ad310fa5926266be42bc9f7988a32f6
Josh Kodroff <jkodroff@weblinc.com>
2014-06-25 14:32:50 -0400

    Add nuget stuff.

--- default.ps1
+++ default.ps1
@@ -11,11 +11,17 @@ task Clean {
     exec { cmd.exe /c "CleanSolution.bat" }
 }
 
-task Compile {
+task Compile -depends RestoreSolutionPackages {
     exec { msbuild SeptaBus.sln }
 }
 
 task Test -depends Compile {
-  exec { & "packages\NUnit.Runners.2.6.3\tools\nunit-console.exe SeptaBus.Core.Tests\bin\Debug\SeptaBus.Core.Tests.dll SeptaBus.StructureMap.Tests\bin\Debug\SeptaBus.StructureMap.Tests.dll  /nologo"   }
+  exec { .\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe SeptaBus.Core.Tests\bin\Debug\SeptaBus.Core.Tests.dll SeptaBus.StructureMap.Tests\bin\Debug\SeptaBus.StructureMap.Tests.dll  /nologo   }
 }
  
+ task Deploy -depends Clean, Compile, Test {
+    exec { del SeptaBus.StructureMap*.nupkg }
+    exec { msbuild SeptaBus.sln "/p:Configuration=Release" }
+    exec { .nuget\nuget.exe pack SeptaBus.StructureMap/SeptaBus.StructureMap.csproj -Prop Configuration=Release }
+    exec { .nuget\nuget.exe push SeptaBus.StructureMap*.nupkg }
+}
